a1c8cd4cd5385a6a7cdd2524ac3893bb2e4a4209
Hadymohammed <Hadymohamed2002@gmail.com>
2022-09-01 02:19:19 +0300

    Build updateGreatings & updateTodayDate functions

---
+++ website/public/js/app.js
@@ -0,0 +1,25 @@
+let today=new Date();
+var options = { weekday: 'short', year: 'numeric', month: 'short' };
+
+function updateGreatings(){
+    let greating=document.getElementById('timeStatus');
+    const hours=today.getHours();
+    if(hours>=0&&hours<12)greating.innerText='morning,';
+    else if(hours>=12&&hours<13)greating.innerText='afternoon,';
+    else greating.innerText='night,';
+    //console.log(greating);
+  //  console.log(hours);
+}
+function updateTodayDate(){
+    let dateSpan=document.getElementById('date');
+    let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(today);
+    let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(today);
+    let da = new Intl.DateTimeFormat('en', { day : '2-digit' }).format(today);
+    //console.log(today.toLocaleDateString("en-US", options));
+    dateSpan.innerText=`: ${da} ${mo}/${ye}`;
+}
+
+function runMain(){
+    updateGreatings();
+    updateTodayDate();
+}
