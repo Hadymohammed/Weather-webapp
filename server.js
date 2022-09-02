b760b21cbeaee93002efc2974f6f02b3678b332c
Hadymohammed <Hadymohamed2002@gmail.com>
2022-09-02 03:07:43 +0300

    Post & get routes Done
    Add get & post routes in server / get data from server side by client side done / post request data to server side / update UI dynamiclay done

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-let projectData={};
-const apiKey='cefa903d59215f0aae790cb5f1c514c1'+'&units=imperial';
+let projectData=[];
+
 // import needed modules
 const http =require('http');
 const express=require('express');
 const bodyParser=require('body-parser');
 const cors=require('cors');
+const fetch=require('node-fetch');
+
 // create an instance of app
 const app=express();
+/* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Initializing the main project folder 
 app.use(express.static('website'));
+
 const port=8000;
 app.listen(port,(req,res)=>{
     console.log(`server running in port : ${port}`);
+});
+
+//Initializing GET route
+app.get('/all/:city',async (req,res)=>{
+    const city=req.params.city;//get city name from client side
+    const apiKey='cefa903d59215f0aae790cb5f1c514c1';
+    const baseURL=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    console.log(baseURL);
+    const fetchedData=await fetch(baseURL);
+    const json = await fetchedData.json();
+    console.log(`request with city : ${city} from server`);
+    //console.log(json);
+    res.json(json);
+});
+
+//Initializing POST route
+app.post('/all',async (req,res)=>{
+    projectData.push(req.body);
+    console.log(projectData);
 })
-
-
--- website/public/js/app.js
+++ website/public/js/app.js
@@ -1,4 +1,3 @@
-const apiKey='cefa903d59215f0aae790cb5f1c514c1'+'&units=imperial';
 let today=new Date();
 var options = { weekday: 'short', year: 'numeric', month: 'short' };
 
@@ -20,13 +19,83 @@ function updateTodayDate(){
     dateSpan.innerText=`: ${da} ${mo}/${ye}`;
 }
 
-function sendData(){
-    const cityInput=document.getElementById('cityEntry');
-    console.log(cityInput.value);
+//GET request from server side
+async function getData(){
+    const timestapm=new Date();
+    //get input city
+    const city=document.getElementById('cityEntry').value;
+
+    //get Divs to display Data
+    const cityNameDiv=document.getElementById('city');
+    const tempMaxDiv=document.getElementById('temp_max');
+    const tempMinDiv=document.getElementById('temp_min');
+    const descDiv=document.getElementById('description');
+
+    //fetch data from server side ///all/// end point
+    //pass city as parameter
+    const data= await fetch(`all/${city}`);
+
+    //convert to json
+    const json= await data.json();
+    console.log(`input city form client side : ${city}`);
+
+    //request succeeded
+    if(json.cod==200){
+    cityNameDiv.innerHTML=json.name;
+    tempMaxDiv.innerHTML=`temp max : ${json.main.temp_max}`;
+    tempMinDiv.innerHTML=`temp max : ${json.main.temp_min}`;
+    descDiv.innerHTML=`status : ${json.weather[0].description}`;
+    console.log(json);
+
+    //save request in history storage in server side
+    sendData('/all',{
+        "date":timestapm.toLocaleDateString(),
+        "time":timestapm.toLocaleTimeString(),
+        "City":json.name,
+        "status":"Succeeded",
+    });
+    }
+    //404 not found
+    else if(json.cod==404) {
+        sendData('/all',{
+            "date":timestapm.toLocaleDateString(),
+            "time":timestapm.toLocaleTimeString(),
+            "City":city,
+            "status":"Failed",
+        });
+        alert("City not found");
+    }
+    /*console.log(json.name);
+    console.log(json.main.temp_max);
+    console.log(json.main.temp_min);*/
+}
+
+//POST request to server side
+async function sendData(url,data){
+    const response = await fetch(url,
+        {
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+       // Body data type must match "Content-Type" header  
+       // put data into request's body      
+        body: JSON.stringify(data), 
+       });
+
+       try {
+        const capsoledData = await response.json();
+        console.log(capsoledData);
+        return capsoledData;
+      }catch(error) {
+      console.log("error", error);
+      }
+    
 }
 function generateClickEvnet(){
     const generateButton=document.getElementById('generate');
-    generateButton.addEventListener('click',sendData);
+    generateButton.addEventListener('click',getData);
 }
 function runMain(){
     updateGreatings();
